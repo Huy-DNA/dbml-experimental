f0752cd275f968f81085024ad76d2c7d9cbb70f3
huydo862003 <huy.do862003@hcmut.edu.vn>
2023-08-08 11:26:45 +0700

    feat: add Option

---
+++ src/lib/option.ts
@@ -0,0 +1,43 @@
+export type Option<T> = Some<T> | None<T>;
+
+export class Some<T> {
+  value: T;
+
+  constructor(value: T) {
+    this.value = value;
+  }
+
+  unwrap(): T {
+    return this.value;
+  }
+
+  unwrap_or<S>(orValue: S): S | T {
+    return this.value;
+  }
+
+  and_then<S>(callback: (_: T) => Option<S>): Option<S> {
+    return callback(this.value);
+  }
+
+  map<S>(callback: (_: T) => S): Option<S> {
+    return new Some(callback(this.value));
+  }
+}
+
+export class None<T> {
+  unwrap() {
+    throw new Error('Trying to unwrap a None value');
+  }
+
+  unwrap_or<S>(orValue: S): S | T {
+    return orValue;
+  }
+
+  and_then<S>(callback: (_: T) => Option<S>): Option<S> {
+    return new None();
+  }
+
+  map<S>(callback: (_: T) => S): Option<S> {
+    return new None();
+  }
+}
