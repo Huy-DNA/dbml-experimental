f1b8454fbc1a386fba7b862ef1270e59f2001340
huydo862003 <huy.do862003@hcmut.edu.vn>
2023-08-11 16:27:50 +0700

    feat: expose serialize

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import Lexer from './lib/lexer/lexer';
 import Parser from './lib/parser/parser';
 import { CompileError } from './lib/errors';
 import Analyzer from './lib/analyzer/analyzer';
+import { serialize } from './lib/serialization/serialize';
 
 export {
- Lexer, Parser, CompileError, Analyzer,
+ Lexer, Parser, CompileError, Analyzer, serialize,
 };
